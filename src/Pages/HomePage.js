bd04f75e4e0b2ef8ac7024017284d6da70703908
DESKTOP-ORRN0CM\Toan Dang <toandang2309@gmail.com>
2023-04-07 20:36:50 +0700

    UPDATE UI Navbar

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,1 +1,14 @@
-//Chưa có gì cả
+import React, { useEffect, useState, useContext } from "react";
+import { Layout, Typography, FloatButton, Image, Pagination, Button, Skeleton, Card } from "antd";
+import { useNavigate } from "react-router-dom";
+import { AppContext } from "../Context/AppProvider";
+import NavBar from "../Components/NavBar";
+import EndBar from "../Components/EndBar";
+import EventCard from "../Components/EventCard";
+import ProductCard from "../Components/ProductCard";
+
+export default function HomePage() {
+    return (
+        <NavBar/>
+    );
+}
